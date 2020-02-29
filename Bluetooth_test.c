cec7787027d14c463081fa51ea82efb3f39af1df
Park-GH <parkgh7124@gmail.com>
2020-02-29 22:52:39 +0900

    Atmega128 HC06 Serial communication test Revert "version 1.0"
    This reverts commit cddc1fc2465e87ab0d13e182f196e337344b181f.

--- Bluetooth_test.c
+++ Bluetooth_test.c
@@ -1,50 +1,35 @@
-#define F_CPU 16000000UL  // 16 MHz
-
-
-
-#include<avr/io.h>
-
-#include<util/delay.h>
-
-
-
-
-
-int main(void)
-
+#define F_CPU 16000000UL // 외부 크리스탈 16MHz
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+char rx_char(void)
 {
-
-	unsigned int	counter=0;
-
-	DDRA	= 0xFF;
-
-	PORTA	= 0x00;
-
-
-
-	while(1)
-
-	{
-
-		if(counter++%2==0)
-
-			PORTA=0x01;
-
-		else
-
-			PORTA=0x00;
-
-
-
-		_delay_ms(200);
-
-	}
-
-	
-
-	return 0;
-
+    while(!(UCSR1A & 0x80));
+    return UDR1;
 }
-
-
-
+ 
+void tx_char(uint8_t bData)
+{
+    while((UCSR1A&0x20) == 0)
+    UDR1 = bData;    
+}
+ 
+ int main(void)
+ {
+    uint8_t bData = 'A';
+    
+    UCSR1A=0x00; //flag 레지스터를 사용하지 않음
+    UCSR1B=0x18; //수신 enable, 송신 enable, 전송비트 8bit
+    UCSR1C=0x06; //비동기식 통신
+    UBRR1H=0;
+    UBRR1L=103; //9600bps
+     
+    /* Replace with your application code */
+    while (1)
+    {
+        tx_char(bData);
+        _delay_ms(2000);
+    }
+     
+     return 0;
+ }
